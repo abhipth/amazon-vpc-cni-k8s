153c4b87cfb00d1d2c6c4d8e00b86686aecd8816
Abhinav Pathak <abhipth@amazon.com>
2021-11-02 11:41:41 -0700

    add more delay in service creation

--- test/integration-new/cni/service_connectivity_test.go
+++ test/integration-new/cni/service_connectivity_test.go
@@ -89,7 +89,7 @@ var _ = Describe("[CANARY] test service connectivity", func() {
 		fmt.Fprintf(GinkgoWriter, "created service\n: %+v\n", service.Status)
 
 		By("sleeping for some time to allow service to become ready")
-		time.Sleep(utils.PollIntervalLong)
+		time.Sleep(utils.PollIntervalLong * 2)
 
 		testerContainer = manifest.NewBusyBoxContainerBuilder().
 			Command([]string{"wget"}).
